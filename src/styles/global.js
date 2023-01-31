c475c87b597dd024a3523e615a4c81e216daa40b
matheusg5467 <119903713+matheusg5467@users.noreply.github.com>
2023-01-31 17:41:22 -0300

    Global style and theme done

---
+++ src/styles/global.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :root {
+        font-size: 62.5%;
+    }
+
+    body {
+        background-color: ${({ theme }) => theme.COLORS.BLACK_BACKGROUND};
+
+        -webkit-font-smoothing: antialiased;
+
+        font-family: 'Dosis', sans-serif;
+        font-weight: 600;
+        font-size: 6.4rem;
+        outline: none;
+    }
+
+    input {
+        font-family: 'Dosis', sans-serif;
+        font-weight: 700;
+        font-size: 4.8rem;
+    }
+
+    button {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover {
+        filter: brightness(1s);
+    }
+`
---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export default {
+    COLORS: {
+        BLACK_BACKGROUND: "#242424",
+        BLACK_TASK_WRAPPER: "#333333",
+
+        WHITE_HIGHLIGHT: "#D4D4D4",
+        WHITE_TASK: "#BABABA",
+
+        GREEN_HEADER: "#2DAE2D",
+        GREEN_SIDEBAR: "#009D00",
+        GREEN_HIGHLIGHT: "#009D00",
+
+    }
+}
