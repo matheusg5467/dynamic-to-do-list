21f39a2bdd7504dd80c9800f8c0385271bd571c1
Matheus G <matheusgvrodrigues@proton.me>
2023-03-10 10:08:06 -0300

    Button color changed for better visibility

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.button`
 	height: 5rem;
 	width: 5rem;
 
-	background-color: ${({ theme }) => theme.COLORS.WHITE_HIGHLIGHT};
+	background-color: ${({ theme }) => theme.COLORS.GREEN_HIGHLIGHT};
 
 	margin: 1.4rem 1.5rem 1.6rem 1.5rem;
 	border-radius: 2rem;
